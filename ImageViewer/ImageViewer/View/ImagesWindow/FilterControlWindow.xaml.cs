a3ddcebd4915d55af40b64fad0fc647462a528af
Tomasz Tarasiewicz <tarasiewicztomasz@gmail.com>
2018-05-25 17:28:37 +0200

    Configuration

---
+++ ImageViewer/ImageViewer/View/ImagesWindow/FilterControlWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ImageViewer.Model.Event;
+using MahApps.Metro.Controls;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageViewer.View.ImagesWindow
+{
+    /// <summary>
+    /// Interaction logic for DisplayImageWindow.xaml
+    /// </summary>
+    public partial class FilterControlWindow : MetroWindow
+    {
+
+        private static FilterControlWindow _instance;
+        protected IEventAggregator _aggregator = GlobalEvent.GetEventAggregator();
+
+        private FilterControlWindow()
+        {
+            InitializeComponent();
+        }
+        public static FilterControlWindow Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new FilterControlWindow();
+                }
+                return _instance;
+            }
+        }
+        public void Dispose()
+        {
+            _instance = null;
+            this.Close();
+        }
+    }
+}
---
+++ ImageViewer/ImageViewer/ViewModel/ImageWindowViewModels/FilterControlViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ImageViewer.Methods;
+using ImageViewer.Model;
+using ImageViewer.Model.Event;
+using ImageViewer.View;
+using ImageViewer.View.ImagesWindow;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageViewer.ViewModel.ImageWindowViewModels
+{
+    public class FilterControlViewModel : BaseViewModel
+    {
+        private short _filterValue = 0;
+        private int _presenterID = 0;
+        public Filter.Filters filter = Filter.Filters.None;
+        public String FilterValue
+        {
+            get
+            {
+                return _filterValue.ToString();
+            }
+            set
+            {
+                _filterValue = Int16.Parse(value);
+                NotifyPropertyChanged();
+                //    FilterEvent fe = new FilterEvent();
+                //    fe.PresenterID = PresenterID;
+                //    fe.Filter = filter;
+                //    fe.Value = _filterValue;
+                //    _aggregator.GetEvent<FilterEvent>().Publish(fe);
+            }
+        }
+        public int PresenterID { get; set; }
+        public FilterControlViewModel()
+        {
+            //_aggregator.GetEvent<FilterEvent>().Subscribe((fe) =>
+            //{
+            //    filter = fe.Filter;
+            //    PresenterID = fe.PresenterID;
+            //    FilterValue = fe.Value.ToString();
+            //});
+        }
+    }
+}
