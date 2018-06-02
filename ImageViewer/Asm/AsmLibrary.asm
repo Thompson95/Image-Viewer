4e878628a273d604d13f99fe42c27673d8e2ff46
Tomasz Tarasiewicz <tarasiewicztomasz@gmail.com>
2018-06-02 23:09:19 +0200

    Negative implemented using SSE registers

--- ImageViewer/Asm/AsmLibrary.asm
+++ ImageViewer/Asm/AsmLibrary.asm
@@ -1,62 +1,63 @@
+.data
+alphaMask byte 4 dup(0FFh, 0FFh, 0FFh, 000h)
+negAlphaMask byte 4 dup(000h, 000h, 000h, 0FFh)
+array255 byte 16 dup(0FFh)
+treshold qword 010h
 .code
 asmNegativeFilter proc
 	mov r10, 0
 	mov r11, rbx
 	mov r9, rdx
-	mov r8, rcx
-	mov rbx, 4
-	jmp loopEnd
-
-	loopStart :
-	mov rdx, 0
-	mov rax, r10
-	div rbx
-	cmp rdx, 3
-	je Alpha
-
-	mov rax, 255
-	sub al, byte ptr[r8 + r10]
-	mov byte ptr[r8 + r10], al
-alpha:
-	inc r10
-loopEnd :
-	cmp r10, r9
-	jl loopStart
-	mov rbx, r11
-
-	ret
-asmNegativeFilter endp
-
-asmBrightnessFilter proc
-	mov r10, 0 
-	mov r11, rbx
-	mov r9, rdx
-	mov rbx, 4
 	jmp loopEnd
 
 loopStart :
-	mov rdx, 0
-	mov rax, r10
-	div rbx
-	cmp rdx, 3
-	je Alpha
-
-	mov rax, 0
-	mov al, byte ptr [rcx + r10]
-	add eax, r8d
-	cmp rax, 255
-	jle noOverflow
-	mov rax, 255
-noOverflow:
-	mov byte ptr[rcx + r10], al
+	vmovupd xmm2, xmmword ptr[rcx + r10]
+	vmovaps xmm1, xmmword ptr[alphaMask]
+	vmovaps xmm0, xmmword ptr[array255]
+	vpsubb xmm3, xmm0, xmm2
+	vpand xmm2, xmm2, xmmword ptr [negAlphaMask]
+	vpaddb xmm3, xmm3, xmm2
+	vmovupd xmmword ptr [rcx + r10], xmm3
 alpha :
-	inc r10
+	add r10, treshold
 loopEnd :
 	cmp r10, r9
 	jl loopStart
 	mov rbx, r11
 
-	ret
+ret
+asmNegativeFilter endp
+
+asmBrightnessFilter proc
+mov r10, 0
+mov r11, rbx
+mov r9, rdx
+mov rbx, 4
+jmp loopEnd
+
+loopStart :
+mov rdx, 0
+mov rax, r10
+div rbx
+cmp rdx, 3
+je Alpha
+
+mov rax, 0
+mov al, byte ptr[rcx + r10]
+add eax, r8d
+cmp rax, 255
+jle noOverflow
+mov rax, 255
+noOverflow:
+mov byte ptr[rcx + r10], al
+alpha :
+inc r10
+loopEnd :
+cmp r10, r9
+jl loopStart
+mov rbx, r11
+
+ret
 asmBrightnessFilter endp
 
 end
