5bdc50f09a3fb7f182b631959aa05108b4455c73
gilotyna815@gmail.com <sluggyfreelance>
2018-07-19 11:34:01 +0200

    Obsolete code removed. Checked looping. Sepia filter now works on SSE registers.

--- ImageViewer/Asm/AsmLibrary.asm
+++ ImageViewer/Asm/AsmLibrary.asm
@@ -112,17 +112,12 @@ mainloop :
 asmContrastFilter endp
 
 
-; several bits must be processed at once
-; best fill xmm1 with R values, xmm2 with G values and xmm3 with B values and process them like that
 asmSepiaFilter proc
 	; parameters processing
 	mov r9, rdx		; size
 	mov r11, rbx	; bitmap
 
 	; setup
-	;mov r13, three
-	;vmovupd xmm4, three
-	cvtdq2pd xmm4, xmm4
 	mov r10, 0			; current position in bitmap
 
 	jmp loopEnd
@@ -130,32 +125,15 @@ asmSepiaFilter proc
 loopStart :
 	sub r10, treshold ; there is enough bits left to be processed
 
-	; xmm1 ; red
-	; xmm2 ; green
-	; xmm3 ; blue
-	vpsubb xmm1, xmm1, xmm1 ; zeroing register for red
-	vpsubb xmm2, xmm2, xmm2 ; zeroing register for green
-	vpsubb xmm3, xmm3, xmm3 ; zeroing register for blue
-	; the sequence in which the colours are coming is actually blue, green, red, alpha
-	; alpha is ignored both in extraction and rewriting of colour so it stays the same
-
-	vmovups xmm0, xmmword ptr[rcx+r10] ; taking 16 bytes from the bitmap - that's 4 bits, each having R, G, B and alpha value
-
-	; diagram is on the photo in the phone
-	; use masks to transform xmm0 into xmm1, xmm2 and xmm3 containing R, G and B
-	; shift R and G to be in the same column as B
-	; add, there is now space for overflow
-	; divide sums by 3 - division takes a lot of time and memory so its good to do it only once
-	; copy and shift the value back into all RGB values in xmm0
-	; using masks detect where R and G are greater than thresholds and set them to 255 there
-	; when implemented in xmm try to implement in ymm
+	vmovups xmm0, xmmword ptr[rcx+r10] ; taking 16 bytes from the bitmap - that's 4 bits per pixel, values RGBA
+	; actual sequence of values in register: ARGB ARGB ARGB ARGB
 
 	vmovups xmm1, xmm0
 	vmovups xmm2, xmm0
 	vmovups xmm3, xmm0
 	vmovups xmm4, xmm0
 
-	vmovups xmm5, xmmword ptr[negAlphaMask] ; inconsistent naming scheme stems from different teams working on different filters
+	vmovups xmm5, xmmword ptr[negAlphaMask] ; inconsistent naming scheme stems from different teams working on different filters, but this is the exact mask needed here so we'll take it
 	vmovups xmm6, xmmword ptr[redMask]
 	vmovups xmm7, xmmword ptr[greenMask]
 	vmovups xmm8, xmmword ptr[blueMask]
@@ -175,7 +153,6 @@ loopStart :
 
 	cvtdq2ps xmm0, xmm0
 	vdivps xmm0, xmm0, xmm3
-	;cvtps2dq xmm0, xmm0
 	
 	; blue
 	movups xmm4, xmm0
@@ -198,214 +175,20 @@ loopStart :
 	cvtps2dq xmm2, xmm2
 	pslldq xmm2, 2
 	
-	vpaddb xmm0, xmm1, xmm2
-	vpaddb xmm0, xmm0, xmm3
-	vpaddb xmm0, xmm0, xmm4
+	vpaddb xmm0, xmm1, xmm2 ; alpha + red
+	vpaddb xmm0, xmm0, xmm3 ; + green
+	vpaddb xmm0, xmm0, xmm4 ; + blue
+	; xmm0 is ready to be returned to bitmap
 
-	; xmm is ready to be returned to bitmap
+	vmovups xmmword ptr[rcx+r10], xmm0 ; returns modified pixels back
 
-	;
-	;
-	;
-	; this is sisd, we're doing simd
-
-	vpminub xmm4, xmm1, xmm7
-	psrldq xmm1, 4
-	vpminub xmm3, xmm1, xmm7
-	psrldq xmm1, 4
-	vpminub xmm2, xmm1, xmm7
-	psrldq xmm1, 4
-
-	vdivps xmm1, xmm1, xmm5
-	vdivps xmm2, xmm2, xmm5
-	vdivps xmm3, xmm3, xmm5
-	vdivps xmm4, xmm4, xmm5
-
-	pslldq xmm2, 4
-	pslldq xmm3, 8
-	pslldq xmm4, 12
-
-	addps xmm1, xmm2
-	addps xmm1, xmm3
-	addps xmm1, xmm4
-
-	;
-	;
-	; obsolete code
-	; blue
-	push rax
-	push rdx
-	pextrb rax, xmm0, 0
-	div three ; now works, but better to it as detailed above
-	pinsrb xmm3, rax, 0
-	pop rdx
-	pop rax
-	; green
-	push rax
-	push rdx
-	pextrb rax, xmm0, 1
-	div three
-	pinsrb xmm2, rax, 0
-	pop rdx
-	pop rax
-	; red
-	push rax
-	push rdx
-	pextrb rax, xmm0, 2
-	div three
-	pinsrb xmm1, rax, 0
-	pop rdx
-	pop rax
-
-	; blue
-	push rax
-	push rdx
-	pextrb rax, xmm0, 4
-	div three
-	pinsrb xmm3, rax, 1
-	pop rdx
-	pop rax
-	; green
-	push rax
-	push rdx
-	pextrb rax, xmm0, 5
-	div three
-	pinsrb xmm2, rax, 1
-	pop rdx
-	pop rax
-	; red
-	push rax
-	push rdx
-	pextrb rax, xmm0, 6
-	div three
-	pinsrb xmm1, rax, 1
-	pop rdx
-	pop rax
-
-	; blue
-	push rax
-	push rdx
-	pextrb rax, xmm0, 8
-	div three
-	pinsrb xmm3, rax, 2
-	pop rdx
-	pop rax
-	; green
-	push rax
-	push rdx
-	pextrb rax, xmm0, 9
-	div three
-	pinsrb xmm2, rax, 2
-	pop rdx
-	pop rax
-	; red
-	push rax
-	push rdx
-	pextrb rax, xmm0, 10
-	div three
-	pinsrb xmm1, rax, 2
-	pop rdx
-	pop rax
-
-	; blue
-	push rax
-	push rdx
-	pextrb rax, xmm0, 12
-	div three
-	pinsrb xmm3, rax, 3
-	pop rdx
-	pop rax
-	; green
-	push rax
-	push rdx
-	pextrb rax, xmm0, 13
-	div three
-	pinsrb xmm2, rax, 3
-	pop rdx
-	pop rax
-	; red
-	push rax
-	push rdx
-	pextrb rax, xmm0, 14
-	div three
-	pinsrb xmm1, rax, 3
-	pop rdx
-	pop rax
-
-	vpaddb xmm1, xmm1, xmm2
-	vpaddb xmm1, xmm1, xmm3
-
-	;
-	;
-	; old code and various tests and experiments from now on
-	vmovupd xmm0, xmmword ptr[rcx+r10] ; blue
-	pextrb r12, xmm0, 0
-	pextrb r13, xmm0, 4
-	pextrb r14, xmm0, 8
-	pextrb r15, xmm0, 12
-	movd xmm3, r12
-
-	movd xmm2, r12
-	vpaddb xmm1, xmm1, xmm2
-	add r10, colour ; red to alpha
-	vmovupd xmm0, xmmword ptr[rcx+r10] ; alpha
-	pextrb r12, xmm0, 0
-	pextrb r13, xmm0, 1
-	pextrb r14, xmm0, 2
-	pextrb r15, xmm0, 3
-	movd xmm2, r12
-	cvtdq2ps xmm2, xmm2		;
-	cvtdq2ps xmm3, xmm3		; cvtdq2ps and cvtps2dq are used to multiply registers using mulps
-	mulps xmm2, xmm3		; otherwise mulps returns 0 regardless of contents of registers
-	cvtps2dq xmm2, xmm2		;
-	vpaddb xmm1, xmm1, xmm2
-
-	add r10, colour ; blue to green
-	vmovupd xmm1, xmmword ptr[rcx+r10] ; green
-	add r10, colour ; green to red
-	vmovupd xmm3, xmmword ptr[rcx+r10] ; red
-	add r10, colour ; red to alpha
-	vmovupd xmm2, xmmword ptr[rcx+r10] ; alpha
-
-	vaddpd xmm0, xmm0, xmm1
-	vaddpd xmm0, xmm0, xmm2
-	vmovups xmm3, val3
-	vdivpd xmm0, xmm0, xmm3
-	;vmovups xmm0, val255
-	vmovupd xmmword ptr [rcx + r10], xmm3 ; blue
-	sub r10, colour ; blue to green
-	vmovupd xmmword ptr [rcx + r10], xmm2 ; green
-	sub r10, colour ; green to red
-	vmovupd xmmword ptr [rcx + r10], xmm1 ; red
-	vmovupd xmm1, xmm0
-	vmovups xmm3, limit
-	cmpps xmm1, xmm3, 6
-	jl tooMuchGreen
-greensGood : 
-	vmovupd xmmword ptr[rcx + r10], xmm1 ; green
-	sub r10, colour ; green to red
-	vmovupd xmm1, xmm0
-	vmovups xmm3, limit
-	cmpps xmm1, xmm3, 6
-	jl tooMuchRed
-redsGood :
-	vmovupd xmmword ptr[rcx + r10], xmm1 ; red
-
-	add r10, treshold	; should be treshold!!
+	add r10, treshold
 loopEnd :
-	add r10, treshold	; test if there is 16 more bits in image
+	add r10, treshold	; test if there is 16 more bits in image (4 pixels)
 	cmp r10, r9
 	jle loopStart		; is yes, loop
 	mov rbx, r11		; if not, end processing the image
-	ret					; this will leave between 0 to 15 last bits unprocessed if number of bits in the bitmap is not a multiple of 16
-
-tooMuchGreen :
-	;vmovups xmm1, val255 ; pshufd xmm3, xmm3, 00h ???
-	jmp greensGood
-
-tooMuchRed :
-	;vmovups xmm1, val255
-	jmp redsGood
+	ret					; this will leave between 0 to 15 last bits unprocessed if number of bits in the bitmap is not a multiple of 16 (4 pixels)
 
 asmSepiaFilter endp
 end
